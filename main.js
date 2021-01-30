d7e6bc45b3754c21987f536a585c0e44af6a80b3
jdhofmann <justindhofmann@gmail.com>
2021-01-30 10:11:02 -0500

    initial

---
+++ main.js
@@ -0,0 +1,20 @@
+// console.log("loaded js")
+
+let burgerBtn = document.querySelector(".burger-btn")
+
+let nav = document.querySelector("nav")
+
+burgerBtn.addEventListener( "click", () => {
+    nav.classList.remove("nav-hidden")
+} )
+
+const scrollLinks = ( i=0 ) => {
+    let links = document.querySelectorAll(".link")
+    links.forEach( lx => lx.classList.remove("in-view"))
+    if(i === links.length ) i = 0;
+    links[i].classList.add("in-view")
+    i+=1;
+    setTimeout(() => scrollLinks(i), 7000)
+}
+
+scrollLinks()
